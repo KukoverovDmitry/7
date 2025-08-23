0ff30f1c83c03446eb50856af848cf194b4d71fb
KukoverovDmitry <moby_dik@mail.ru>
2025-08-23 11:23:38 +0300

    1

---
+++ reminder.py
@@ -0,0 +1,21 @@
+import pygame
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import time
+
+window = Tk()
+window.title("Reminder")
+
+label = Label(window, text="Enter your reminder")
+label.pack(pady=10)
+
+set_button = Button(window, text="Set Reminder", command= set))  
+set_button.pack(pady=10)
+
+
+
+
+
+window.mainloop()
