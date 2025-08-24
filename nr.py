bad5c0e677bc33a7780ae9ae12e952d97c097f70
KukoverovDmitry <moby_dik@mail.ru>
2025-08-24 10:51:45 +0300

    добвалили функция проверки

---
+++ nr.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import messagebox as mb
+import random
+
+window = Tk()
+window.title("Игра угадай число")
+
+m1 = Label(window, text="Угадай число от 1 до 100")
+m1.pack()
+
+
+def check():
+     s = e.get()
+     rnd  = random.randint(1, 2)
+     if s == rnd:
+          mb.showinfo(title="Угадал", message="Вы угадали")
+     else:
+         mb.showinfo(title="Не угадал", message="Вы не угадали", icon="warning")
+    
+     answer = mb.askretrycancel(title="Вопрос", message="Еще раз?")         
+ 
+     if answer:
+        e.delete(0, END)
+     else:
+        window.destroy()
+
+e = Entry(window)
+e.pack()
+
+
+b = Button(window, text="Передать", command=check)
+b.pack()
+
+m = Label(window, height=3, width=30    )
+m.pack()
+
+
+window.mainloop()
