80fa2bbe32d480d2ccdf4515deda828b17f3f191
KukoverovDmitry <moby_dik@mail.ru>
2025-08-24 12:13:33 +0300

    остановка музыки

--- reminder.py
+++ reminder.py
@@ -4,23 +4,24 @@ from tkinter import messagebox as mb
 from tkinter import simpledialog as sd
 import datetime
 import time
-tT = 0
+t = 0
+music = False
 def set():
     global t
     rem = sd.askstring("Время напомнинания"
-                       , "Введите время напоминания в формате ЧЧ:ММ(в 24 часовом формате )")
+                       , "Введите время напоминания в формате ЧЧ:ММ(в 24 часовом формате )",)
     if rem:
         try:
             hour = int(rem.split(":")[0])
             minute = int(rem.split(":")[1])
             now = datetime.datetime.now()
             print(now)
-            dr = now.replace(hour=hour, minute=minute, second=0)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             print(dt)
 
             t = dt.timestamp()
             print(t)
-
+            
         except Exception as e:
             mb.showerror("Ошибка", f"Ошибка: {e}")
             
@@ -36,21 +37,34 @@ def check():
 
 
 def play_snd():
+    global music
+    music = True
     pygame.mixer.init()
-    pygame.mixer.music.load("reminder.mp3")
-
+    pygame.mixer.music.load("1.mp3")
+    pygame.mixer.music.play()
+def stop():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text="Новое напоминание")    
 
 window = Tk()
 window.title("Reminder")
+window.geometry("300x200+200+200")
 
-label = Label(window, text="Enter your reminder")
+label = Label(window, text="Enter your reminder", font=("Arial", 20))
 label.pack(pady=10)
 
 set_button = Button(window, text="Set Reminder", command= set)
 set_button.pack(pady=10)
+stop_button = Button(window, text="Stop Music", command= stop)
+stop_button .pack(pady=10)  
 
+stop_programm = Button(window, text="Stop Programm", background="red",  command= window.destroy)
+stop_programm.pack(pady=10)
 
-
+check()
 
 
 window.mainloop()
